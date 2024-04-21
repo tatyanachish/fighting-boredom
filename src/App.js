2adff1b5d68b78c9a08dd148cf68fd6d8ed6edf0
tatyanachish <tatyanachishkina@gmail.com>
2024-04-20 22:19:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [advice,setAdvice] = useState ('');
+  useEffect(()=> {
+    const getAdvice = async () => {
+      const responce = await fetch('http://www.boredapi.com/api/activity/');
+      const data = await responce.json();
+      console.log(data.activity)
+      setAdvice(data.activity)
+        }
+    getAdvice();
+  }, [])
+  // useEffect (()=>{
+  //   getAdvice();
+  // }, [])
+  // const getAdvice = async () => {
+  //   const responce = await fetch('http://www.boredapi.com/api/activity/');
+  //   const data = await responce.json();
+  //   console.log(data.activity)
+  //   setAdvice(data.activity)
+  // }
 
+return (
+<div>
+  <div className='btn'>
+    <button onClick = {getAdvice}>Click here and get advice</button>
+  </div>
+  <div className='btn'>
+      <p>{advice}</p>
+  </div>
+</div>
+
+);
+
+}
 export default App;
